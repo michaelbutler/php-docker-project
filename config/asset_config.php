a08be22ca8d263dc5afec6d6c0d2bc08a00c8e25
Michael Butler <mbutler@tumblr.com>
2023-12-15 10:41:24 -0500

    Automated asset compilation

---
+++ config/asset_config.php
@@ -0,0 +1,13 @@
+<?php 
+
+return array (
+  'scripts' => 
+  array (
+    0 => '/dist/js/runtime.29aabd80e5a38e754f50.bundle.js',
+    1 => '/dist/js/main.64461c0a78c25ba82e57.bundle.js',
+  ),
+  'css' => 
+  array (
+    0 => '/dist/styles/main.31c731f3923c5b2a6749.css',
+  ),
+);
